3f1ca7bf102b756086676617725cae9d20974fba
moimart1 <moimart1@hotmail.fr>
2022-11-13 18:54:29 -0500

    Add share in transaction entity

--- src/endpoints/transactions/entities/transaction.entity.ts
+++ src/endpoints/transactions/entities/transaction.entity.ts
@@ -1,5 +1,26 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { Contact, ContactSchema } from '../../contacts/entities/contact.entity';
+
+@Schema({ _id: false })
+export class TransactionShare {
+  @Prop({ type: ContactSchema })
+  contact: string | Contact;
+
+  /**
+   * Amount paid by the contact
+   */
+  @Prop({ type: Number })
+  paid: number;
+
+  /**
+   * Amount that the contact need to pay of the transaction
+   */
+  @Prop({ type: Number })
+  owed: number;
+}
+
+const TransactionShareSchema = SchemaFactory.createForClass(TransactionShare);
 
 @Schema({ timestamps: true })
 export class Transaction {
@@ -18,9 +39,6 @@ export class Transaction {
   @Prop({ default: 'CAD' })
   currency: string;
 
-  @Prop({ default: 'martin' })
-  author: string;
-
   @Prop({ default: false })
   reviewed: boolean;
 
@@ -29,6 +47,9 @@ export class Transaction {
 
   @Prop() // TODO
   source: string;
+
+  @Prop({ type: [TransactionShareSchema] })
+  shares: TransactionShare[];
 }
 
 export type TransactionDocument = Transaction & Document;
